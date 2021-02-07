ca12bea55974933d47a2a038bfe5627943dbcb22
EarleyBird7 <72363693+EarleyBird7@users.noreply.github.com>
2021-02-07 14:59:13 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+var database;
+var form;
+var player;
+var game;
+var gameState=0;
+var playerCount;
+var allPlayers;
+
+function setup(){
+  database = firebase.database();
+  createCanvas(400,400);
+game = new Game()
+game.getState();
+game.start();
+  
+}
+
+function draw(){
+  if(playerCount===4){
+    game.update(1)
+  }
+  if (gameState===1){
+    clear()
+    game.play()
+  }
+}
+   
+  
+
+
+
